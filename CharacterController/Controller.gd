b302dee9ff4feaac070beb1e55b42ae99807dd4e
mvann <michaelmvann@gmail.com>
2020-11-03 14:05:51 -0800

    added player collision
    Added a KinematicBody2d to player. Also refactored the code a bit to make it more logically organized.

--- CharacterController/Controller.gd
+++ CharacterController/Controller.gd
@@ -1,49 +1,56 @@
-extends Area2D
-signal hit
+extends KinematicBody2D
 
 const IDLE = "idle"
 const WALKING = "walking"
 const WALKING_UP = "walkingUp"
 const WALKING_DOWN = "walkingDown"
 
+const UI_RIGHT = "ui_right"
+const UI_LEFT = "ui_left"
+const UI_DOWN = "ui_down"
+const UI_UP = "ui_up"
+
 export var switch_diagonal_animation = false
 export var speed = 400
 var screen_size
 var heading = Vector2()
+var velocity = Vector2()
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _process(delta):
-	move(delta)
+	update_heading()
+	update_animations()
+	update_velocity()
 
-func move(delta):
-	var velocity = Vector2()
-	
+func _physics_process(delta):
+	move_and_slide(velocity) # don't multiply by delta for move_and_slide
+
+func update_heading():
 	# calculate X heading
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed(UI_RIGHT):
 		heading.x = 1
-	elif Input.is_action_just_released("ui_right"):
-		heading.x = -1 if Input.is_action_pressed("ui_left") else 0
-	if Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_just_released(UI_RIGHT):
+		heading.x = -1 if Input.is_action_pressed(UI_LEFT) else 0
+	if Input.is_action_just_pressed(UI_LEFT):
 		heading.x = -1
-	elif Input.is_action_just_released("ui_left"):
-		heading.x = 1 if Input.is_action_pressed("ui_right") else 0
+	elif Input.is_action_just_released(UI_LEFT):
+		heading.x = 1 if Input.is_action_pressed(UI_RIGHT) else 0
 	# calculate Y heading
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_just_pressed(UI_DOWN):
 		heading.y = 1
-	elif Input.is_action_just_released("ui_down"):
-		heading.y = -1 if Input.is_action_pressed("ui_up") else 0
-	if Input.is_action_just_pressed("ui_up"):
+	elif Input.is_action_just_released(UI_DOWN):
+		heading.y = -1 if Input.is_action_pressed(UI_UP) else 0
+	if Input.is_action_just_pressed(UI_UP):
 		heading.y = -1
-	elif Input.is_action_just_released("ui_up"):
-		heading.y = 1 if Input.is_action_pressed("ui_down") else 0
-	
-	# calculate animations and velocity
+	elif Input.is_action_just_released(UI_UP):
+		heading.y = 1 if Input.is_action_pressed(UI_DOWN) else 0
+
+func update_animations():
 	if heading != Vector2.ZERO:
 		if heading.x != 0:
 			$AnimatedSprite.flip_h = heading.x < 0
-		
 		if switch_diagonal_animation:
 			if heading.y != 0:
 				$AnimatedSprite.animation = WALKING_UP if heading.y < 0 else WALKING_DOWN
@@ -54,18 +61,11 @@ func move(delta):
 				$AnimatedSprite.animation = WALKING
 			else:
 				$AnimatedSprite.animation = WALKING_UP if heading.y < 0 else WALKING_DOWN
-		
-		velocity = heading.normalized() * speed
 	else:
 		$AnimatedSprite.animation = IDLE
-		# velocity is already Vector2.ZERO
-	
-	# apply velocity
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
 
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+func update_velocity():
+	if heading != Vector2.ZERO:
+		velocity = heading.normalized() * speed
+	else:
+		velocity = Vector2.ZERO
