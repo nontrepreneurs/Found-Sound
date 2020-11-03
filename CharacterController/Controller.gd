051be3c71bf5860883cf6dfdcf5dd427b172cbc9
mvann <michaelmvann@gmail.com>
2020-11-02 22:13:48 -0800

    changed input map
    changed input map and cleaned up some code

--- CharacterController/Controller.gd
+++ CharacterController/Controller.gd
@@ -1,18 +1,16 @@
 extends Area2D
 signal hit
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export var speed = 400
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	# hide()
 	
 func _process(delta):
+	move(delta)
+
+func move(delta):
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
@@ -30,23 +28,10 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
-		#$AnimatedSprite.flip_v = false
 		$AnimatedSprite.flip_h = velocity.x < 0
-	#elif velocity.y != 0:
-	#	$AnimatedSprite.animation = "up"
-	#	$AnimatedSprite.flip_v = velocity.y > 0
 
-func move(delta):
-	pass
-
-#func _on_Player_body_entered(body):
-#	hide()
-#	emit_signal("hit")
-#	$CollisionShape2D.set_deferred("disabled", true)
-	
 func start(pos):
 	position = pos
 	show()
