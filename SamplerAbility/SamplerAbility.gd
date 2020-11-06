7f81cd2504c350e04c500785975f7defa7c55b64
mvann <michaelmvann@gmail.com>
2020-11-05 19:33:28 -0800

    Started ripple

---
+++ SamplerAbility/SamplerAbility.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var is_recording = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func record_sound():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
