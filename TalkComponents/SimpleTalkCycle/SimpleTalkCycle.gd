3e07fa647ec07f1a8a1418900e69641f79d1495b
mvann <michaelmvann@gmail.com>
2020-11-29 14:19:06 -0800

    added simple talk cycle

--- TalkComponents/SimpleTalkCycle/SimpleTalkCycle.gd
+++ TalkComponents/SimpleTalkCycle/SimpleTalkCycle.gd
@@ -1,11 +1,10 @@
 extends Node
 
-export var dialogues: PoolStringArray = []
+export(Array, PoolStringArray) var dialogues = null
 onready var talk_radius: TalkRadius = get_node("../TalkRadius")
 var dialogue_index = 0
 
 const ON_TALK_SIGNAL = "on_talk_signal"
-
 
 func _ready():
 	assert(dialogues.size() > 0, "ERROR: SimpleTalkCycle requires at least one dialogue.")
@@ -14,5 +13,8 @@ func _ready():
 	talk_radius.connect(talk_radius.TALK_SIGNAL, self, ON_TALK_SIGNAL)
 
 func on_talk_signal():
+	var tmp_dialogues = []
+	for dia_string in dialogues[dialogue_index]:
+		tmp_dialogues.append(Dialogue.new(dia_string))
+	DialogueManager.loadDialogue(tmp_dialogues)
 	dialogue_index = (dialogue_index + 1) % dialogues.size()
-	DialogueManager.loadDialogue([Dialogue.new(dialogues[dialogue_index])])
--- TalkRadius/TalkRadius.gd
+++ TalkRadius/TalkRadius.gd
@@ -3,13 +3,14 @@ class_name TalkRadius
 
 onready var label: Label = $Label
 var talkable = false
+var someone_is_talking: Resource = load("res://Tidbits/SomeoneIsTalking.tres")
 
 const TALK_ACTION = "confirm"
 const TALK_SIGNAL = "talk_signal"
 signal talk_signal()
 
 func _input(event):
-	if talkable and event.is_action_pressed(TALK_ACTION):
+	if talkable and event.is_action_pressed(TALK_ACTION) and !someone_is_talking.value:
 		emit_signal(TALK_SIGNAL)
 
 func _on_Area2D_body_entered(body):
