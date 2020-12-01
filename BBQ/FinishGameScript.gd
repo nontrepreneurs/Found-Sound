8d7ebb28ceace70a798fefae4fa9f586632fe87a
mvann <michaelmvann@gmail.com>
2020-11-30 19:10:22 -0800

    almost done

--- BBQ/FinishGameScript.gd
+++ BBQ/FinishGameScript.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 onready var talk_radius: TalkRadius = get_node("../TalkRadius")
-#onready var end_screen = $EndScreen
 
 const ON_TALK_SIGNAL = "on_talk_signal"
 
-func _ready():	
+func _ready():
 	assert(talk_radius, "ERROR: FinishGameScript requires TalkRadius sibling.")
 	talk_radius.connect(talk_radius.TALK_SIGNAL, self, ON_TALK_SIGNAL)
 
 func on_talk_signal():
+	get_node("../../../../CanvasLayer/ColorRect").visible = true
+	SoundtrackManager.transitionSong(Constants.Soundtrack.VICTORY)
 	print("GAME OVER MAN")
 #	end_screen.visible = true
